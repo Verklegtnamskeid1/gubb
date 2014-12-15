5cfc50f5c398a82a87b7a14850528912e8e67d5c
netnotandi <jonthor84@gmail.com>
2014-12-15 17:17:02 +0000

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -13,18 +13,19 @@ database::database(QWidget *parent) :
     ui->setupUi(this);
     ui->Search_edit->setPlaceholderText("Search");
   //  setCentralWidget(ui->tableWidget);  //setur þannig að tablewidget tekur mest alt plássið
-
 }
 
 database::~database()
 {
     delete ui;
 }
-//void database::createToolBars()
-  //  {
-  /*  //    fileToolBar = addToolBar(tr("File"));
-        fileToolBar->addAction(newAct);
-}*/
+
+void database::on_Search_edit_cursorPositionChanged(int arg1, int arg2)
+{
+    //displaydatabase-ið sem við ætlum að nota();
+}
+
+
 void database::on_actionAdd_a_new_computer_triggered()
 {
    insertcomp = new Insertcomputer(this);
@@ -48,29 +49,6 @@ void database::on_actionAdd_a_new_connection_triggered()
 
 void database::on_tableWidget_activated(const QModelIndex &index)
 {
-    ui->display_all->clear();
-
 
 }
 
-
-void database::on_treeWidget_activated(const QModelIndex &index)
-{
-
-}
-
-void database::on_Search_edit_textChanged(const QString &arg1)
-{
-
-}
-
-void database::displayCurrentPersons() {
-    ui->personTableWidget->setRowCount(currentPersons.size());
-    ui->personTableWidget->clearContents();
-    for (unsigned int i = 0; i < currentPersons.size(); i++) {
-        ui->personTableWidget->setItem(i, 0, new QTableWidgetItem(QString::fromStdString(currentPersons[i].getName())));
-        ui->personTableWidget->setItem(i, 1, new QTableWidgetItem(QString::fromStdString(currentPersons[i].getBirthyear())));
-        ui->personTableWidget->setItem(i, 2, new QTableWidgetItem(QString::fromStdString(currentPersons[i].getDeathyear())));
-        ui->personTableWidget->setItem(i, 3, new QTableWidgetItem(QString::fromStdString(currentPersons[i].getGender())));
-    }
-}
